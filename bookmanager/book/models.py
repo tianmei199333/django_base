9efc3316e8c17641e5cc87c97f293373543a3c43
tianmei199333 <thaitriptimea@163.com>
2022-07-17 18:53:40 +0800

    定义模型

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+定义模型：
+
+1. 我们的的模型类， 需要继承自  model.Model
+2. 系统会自动 我们添加一个主键 -- id
+3. 字段
+    
+    字段名=model.类型（选项）
+    字段名其实就是数据表的字段名
+    字段名不要使用 python，  mysql等关键字
+    char(M)
+    varchar(M)
+    M 就是选项
+"""
+
+
+class BookInfo(models.Model):
+    # id
+    name = models.CharField(Max_length=10)
+
+
+# 人物 先复制过来，后期讲 原理
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    # 外键约束： 人物属于哪本书
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -37,10 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'book', solution 1
-    # 'book.apps.BookConfig', # solution 2
-    #
-     ]
+    'book'  # 方案1
+    # 'book.apps.BookConfig', # 方案2
+]
 
 
 MIDDLEWARE = [
